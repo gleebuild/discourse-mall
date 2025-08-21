7f2d4d6942d4a03ea33a33eafe117eb205672f4d
gleebuild <gleebuild@gmail.com>
2025-08-22 02:38:16 +0800

    Delete app directory

--- app/models/discourse_mall/coupon.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-module DiscourseMall
-  class Coupon < ActiveRecord::Base
-    self.table_name = "discourse_mall_coupons"
-    enum :discount_type, { amount: 0, percent: 1 }, default: :amount
-    validates :code, presence: true, uniqueness: true
-  end
-end
