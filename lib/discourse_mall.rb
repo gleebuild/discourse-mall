b73e553516a2e72312f82a932f13ff371421da13
gleebuild <gleebuild@gmail.com>
2025-08-22 02:26:55 +0800

    Delete lib directory

--- lib/discourse_mall.rb
+++
@@ -1,9 +0,0 @@
-
-# frozen_string_literal: true
-require_relative 'discourse_mall/engine'
-require_relative '../app/controllers/discourse_mall/public_controller'
-require_relative '../app/controllers/discourse_mall/admin_controller'
-require_relative '../app/models/discourse_mall/product'
-require_relative '../app/models/discourse_mall/variant'
-require_relative '../app/models/discourse_mall/coupon'
-require_relative '../app/models/discourse_mall/order'
