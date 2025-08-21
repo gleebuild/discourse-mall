88299a44d0c7544f4b4e84f0cdf505a7a5f8a1d8
gleebuild <gleebuild@gmail.com>
2025-08-22 02:26:21 +0800

    Delete app directory

--- app/models/discourse_mall/variant.rb
+++
@@ -1,8 +0,0 @@
-
-# frozen_string_literal: true
-class DiscourseMall::Variant < ActiveRecord::Base
-  self.table_name = 'discourse_mall_variants'
-  belongs_to :product, class_name: 'DiscourseMall::Product'
-  validates :title, presence: true
-  validates :price_cents, numericality: { greater_than_or_equal_to: 0 }
-end
